92cdb51ce20b80c401cc7d6a2bb67709fcc0e577
Giacomo Melacini <gmelacini@unibz.it>
2022-10-19 12:27:50 +0200

    Added write_sol and write_stat_line function

--- utils/utils.py
+++ utils/utils.py
@@ -2,8 +2,10 @@ import os
 from random import randint
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
+import csv
 
-#load data and convert it into readable types
+
+# load data and convert it into readable types
 def load_data(index: int):
     """
     Parse and load instances in order to be compatible with minizinc data type
@@ -20,7 +22,7 @@ def load_data(index: int):
     w: int
         The plate width
     n: int
-        The number of chips 
+        The number of chips
     widths: list
         the widths of the chips
     heights: list
@@ -49,14 +51,15 @@ def load_data(index: int):
         else:
             chip_wh = line.split()
             widths.append(int(chip_wh[0]))
-            heights.append(int(chip_wh[1]))            
+            heights.append(int(chip_wh[1]))
 
         count += 1
-    
+
     return w, n, widths, heights
 
-#plot the images
-def plot_device(pos_x: list, pos_y: list, widths: list, heights: list, w: int, h: int, img_path: str = ""):  
+
+# plot the images
+def plot_device(pos_x: list, pos_y: list, widths: list, heights: list, w: int, h: int, img_path: str = ""):
 
     """
     Create and save an image of the solution produced
@@ -75,11 +78,11 @@ def plot_device(pos_x: list, pos_y: list, widths: list, heights: list, w: int, h
     w: int
         The plate width
     h: int
-        The plate height 
+        The plate height
     img_path: str
         The path where the image will be saved, if not specified the image will be shown but not saved
 
-    """  
+    """
 
     fig, ax = plt.subplots()
     ax.axis([0, w, 0, h])
@@ -100,7 +103,8 @@ def plot_device(pos_x: list, pos_y: list, widths: list, heights: list, w: int, h
 
     fig.clf()
 
-#plot the image for rotation
+
+# plot the image for rotation
 def plot_device_rotation(pos_x: list, pos_y: list, widths: list, heights: list, w:int, h: int, rotations: list, img_path: str = ""):
 
     """
@@ -120,7 +124,7 @@ def plot_device_rotation(pos_x: list, pos_y: list, widths: list, heights: list, 
     w: int
         The plate width
     h: int
-        The plate height 
+        The plate height
     rotations:
         An array of bool reporting the rotated chips
     img_path: str
@@ -132,4 +136,59 @@ def plot_device_rotation(pos_x: list, pos_y: list, widths: list, heights: list, 
     actual_heights = [(heights[i] * (1-rotations[i])) + (widths[i]*rotations[i]) for i in range(0, len(widths))]
 
     plot_device(pos_x, pos_y, actual_widths, actual_heights, w, h, img_path)
+
+
+def write_sol(path, w, h, n, widths, heights, pos_x, pos_y):
+
+    """
+    Save solution in txt format in the format requested by the project description
+
+    Parameters
+    ----------
+    :param path: str
+        The path where the output will be saved
+    :param w: int
+        The plate width
+    :param h: int
+        The plate height
+    :param n: int
+        The number of chips
+    :param widths: list
+        The chip's widths
+    :param heights: list
+        The chip's height
+    :param pos_x: list
+        The x positions of the chips
+    :param pos_y: list
+        The y positions of the chips
+
+    """
+
+    with open(path, 'w') as f:
+        f.write(f'{w} {int(h)}\n{n}\n')
+
+        for i in range(n):
+            f.write(f'{widths[i]} {heights[i]} {int(pos_x[i])} {int(pos_y[i])}\n')
+
+        f.close()
+
+
+def write_stat_line(path, instance, height, time):
+    """
+    Append to the csv file (or create it if file does not exist)
+    the stats related to the solution of a specified instance
+
+    :param path: str
+         The path where to append the stats
+    :param instance: int
+        number of the solved instance
+    :param height: int
+        found height of the silicon plate
+    :param time: float
+        time spent to solve the instance
+
+    """
+    with open(path, 'a') as file:
+        writer = csv.writer(file)
+        writer.writerow([instance, height, time])
 
